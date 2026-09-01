453756a22cb4d4d00ab14a0afb887d03057bc739
Arunkasana001 <arunkasana975@gmail.com>
2026-09-01 13:03:23 +0530

    feat: add AppName component and integrate it into App component

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,7 +1,8 @@
+import AppName from "./components/AppName";
 function App() {
   return (
     <center class="todo-container">
-      <h1>TODO App</h1>
+     <AppName />
       <div class="container text-center">
         <div class="row">
           <div class="col-6"><input type="text" placeholder="Enter Todo here.." /></div>
@@ -14,9 +15,9 @@ function App() {
           <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
         </div>
         <div class="row">
-          <div class="col-6"><input type="text" placeholder="Enter Todo here.." /></div>
-          <div class="col-4"><input type="date" /></div>
-          <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
+          <div class="col-6">Go to College</div>
+          <div class="col-4">4/10/2023</div>
+          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
         </div>
       </div>
     </center>
