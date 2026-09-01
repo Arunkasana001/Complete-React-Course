ffcc3ab5125b41f15fbff64d0ede54b715b093fc
Arunkasana001 <arunkasana975@gmail.com>
2026-09-01 11:00:28 +0530

    feat: implement Random component and update App component to include multiple instances fix: add comment for dynamic components in Hello component refactor: comment out unused CSS import in main file

--- 2 create-react-comp/src/App.jsx
+++ 2 create-react-comp/src/App.jsx
@@ -1,19 +1,20 @@
- import AkButton from "./AkButton";
- import Hello from "./Hello";
- function App(){
-  return <div>
-     <h1>
-    Hello World!</h1>
-    <AkButton></AkButton>
-    <Hello></Hello>
- 
-  </div>
- }
-  
- export default App; 
+import Random from "./Random";
+import AkButton from "./AkButton";
+import Hello from "./Hello";
 
-  
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <AkButton></AkButton>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Hello></Hello>
+    </div>
+  );
+}
 
-
-
- 
+export default App;
--- 2 create-react-comp/src/Hello.jsx
+++ 2 create-react-comp/src/Hello.jsx
@@ -3,6 +3,7 @@ function Hello(){
   let fullName = () =>{
 return 'Arun Kasana'
   }
+  // Dynamic Components'{}'  -- Using {}, we can embed any JS expression directly within JSX.This includes variables, function calls, and more.
   return <h3>
     Hello this is the future speaking. I am your master {fullName()}
   </h3>
