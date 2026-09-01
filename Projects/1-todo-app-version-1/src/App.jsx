9d1d699a9a2f822653ef154d78a67170c57402f9
Arunkasana001 <arunkasana975@gmail.com>
2026-09-01 13:43:08 +0530

    feat: add AddTodo component and integrate it into App component

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,25 +1,14 @@
 import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./TodoItem1";
+import TodoItem2 from "./TodoItem2";
 function App() {
   return (
     <center class="todo-container">
      <AppName />
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6"><input type="text" placeholder="Enter Todo here.." /></div>
-          <div class="col-4"><input type="date" /></div>
-          <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
-        </div>
-        <div class="row">
-          <div class="col-6">Buy Milk</div>
-          <div class="col-4">4/10/2023</div>
-          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
-        </div>
-        <div class="row">
-          <div class="col-6">Go to College</div>
-          <div class="col-4">4/10/2023</div>
-          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
-        </div>
-      </div>
+     <AddTodo />
+      <TodoItem1 />
+     <TodoItem2></TodoItem2>
     </center>
   );
 }
