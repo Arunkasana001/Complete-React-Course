258bf4cb89e39f3f93eaaae7c4c7b5a94c255227
Arunkasana001 <arunkasana975@gmail.com>
2026-09-01 10:43:16 +0530

    feat: add Hello component and integrate it into App component

--- 2 create-react-comp/src/App.jsx
+++ 2 create-react-comp/src/App.jsx
@@ -1,9 +1,11 @@
  import AkButton from "./AkButton";
+ import Hello from "./Hello";
  function App(){
   return <div>
      <h1>
     Hello World!</h1>
     <AkButton></AkButton>
+    <Hello></Hello>
  
   </div>
  }
---
+++ 2 create-react-comp/src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello(){
+  let myName = 'Arun';
+  let fullName = () =>{
+return 'Arun Kasana'
+  }
+  return <h3>
+    Hello this is the future speaking. I am your master {fullName()}
+  </h3>
+}
+
+export default Hello;
