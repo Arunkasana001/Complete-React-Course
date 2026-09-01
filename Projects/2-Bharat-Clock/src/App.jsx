6cb0f89ca00a8f75d7a8b6e50ed3013c5adfd01e
Arunkasana001 <arunkasana975@gmail.com>
2026-09-01 17:24:05 +0530

    feat: add SVG assets for React and Vite, create initial CSS styles, and set up main entry point with Vite configuration

---
+++ Projects/2-Bharat-Clock/src/App.jsx
@@ -0,0 +1,13 @@
+function App(){
+
+  return(
+    <center className="clock-container"> 
+      <h1>Bharat Clock</h1>
+      <div><h2>This is the clock thst shows the time in Bharat at all times</h2></div>
+      <div><h3>This is the Curretnt time : 26/10/2023 - 10:38:14 AM</h3></div>
+    </center>
+  )
+
+}
+  export default App;
+
