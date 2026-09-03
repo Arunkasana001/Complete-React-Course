a783e35742f4187305a64eaf02f95f3294d87d2e
Arunkasana001 <arunkasana975@gmail.com>
2026-09-03 14:18:42 +0530

    feat: add CSS modules for styling components and update class attributes to className

--- Projects/3-todo-app-version-2/src/components/AppName.jsx
+++ Projects/3-todo-app-version-2/src/components/AppName.jsx
@@ -1,4 +1,5 @@
+import styles from "./Heading.modules.css"
 function AppName(){
-return  <h1>TODO  App</h1>
+return  <h1 className={styles['h1']}>TODO  App</h1>
 }
 export default AppName;
--- Projects/3-todo-app-version-2/src/components/TodoItem1.jsx
+++ Projects/3-todo-app-version-2/src/components/TodoItem1.jsx
@@ -7,11 +7,11 @@ function TodoItem1() {
       
 
     
-    <div class=" row Ak-row">
-      <div class="col-6">{todoName}</div>
-      <div class="col-4">{tododate}</div>
-      <div class="col-2">
-        <button type="button" class="btn btn-danger Ak-button item-btn">
+    <div className=" row Ak-row">
+      <div className="col-6">{todoName}</div>
+      <div className="col-4">{tododate}</div>
+      <div className="col-2">
+        <button type="button" className="btn btn-danger Ak-button item-btn">
           Delete
         </button>
       </div>
