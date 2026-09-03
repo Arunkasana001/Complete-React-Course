4f744a9d08c0efddfeda0420d13cb4f1927ad88b
Arunkasana001 <arunkasana975@gmail.com>
2026-09-03 12:57:55 +0530

    feat: remove unused TodoItem components and clean up App structure

--- Projects/3-todo-app-version-2/src/App.jsx
+++ Projects/3-todo-app-version-2/src/App.jsx
@@ -1,7 +1,6 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
+
 import "./App.css";
 import TodoItem from "./components/TodoItem";
 
@@ -12,9 +11,12 @@ function App() {
       <AddTodo />
       <div className="items-container">
         <TodoItem todoName ="Buy Milk" todoDate= "4/10/2023"></TodoItem>
-        <TodoItem1 />
-        <TodoItem2></TodoItem2>
-      </div>
+        </div>
+      <div className="items-container">
+        <TodoItem todoName ="Go to College" todoDate= "4/10/2023"></TodoItem>
+</div>
+       
+    
     </center>
   );
 }
