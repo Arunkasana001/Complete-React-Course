1cd09cff29d0600d6866558f73f7752ec04b436b
Arunkasana001 <arunkasana975@gmail.com>
2026-09-03 20:39:43 +0530

    Add initial setup for React application with Vite
    - Created main entry point (main.jsx) for the React application.
    - Added Vite configuration file (vite.config.js) to set up Vite with React plugin.
    - Included React and Vite logos as SVG assets (react.svg, vite.svg).

---
+++ Projects/4-calculator-version-1/calculator/src/App.jsx
@@ -0,0 +1,21 @@
+import styles from "./App.module.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+
+  return (
+    <div className={styles.calculator}>
+      <input className={styles.display} type="text" />
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>c</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>+</button>
+        <button className={styles.button}>3</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
