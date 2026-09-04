7aad51cc6f86a7fa56abeced339718e85f0c3a52
Arunkasana001 <arunkasana975@gmail.com>
2026-09-04 12:06:38 +0530

    feat: add button to Item component for purchase action

--- 4 Fragments/learning-fragment/src/components/Item.jsx
+++ 4 Fragments/learning-fragment/src/components/Item.jsx
@@ -3,7 +3,9 @@ import styles  from "./Item.module.css";
 const Item = ({foodItems}) =>{
 return (
   <>
-  <li  className={ `${styles['ak-item']} list-group-item`} ><span className={styles['ak-span']} >{foodItems}</span></li>
+  <li  className={ `${styles['ak-item']} list-group-item`} ><span className={styles['ak-span']} >{foodItems}</span>
+  <button className={`${styles.button} btn btn-info`}>Buy</button>
+  </li>
   </>  
 )
 
