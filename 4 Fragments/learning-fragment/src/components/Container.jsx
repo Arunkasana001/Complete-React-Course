14ffdadaf8771304812e79ccb2c46345075a87bd
Arunkasana001 <arunkasana975@gmail.com>
2026-09-04 11:39:10 +0530

    docs: add comment to clarify usage of props.children in Container component

--- 4 Fragments/learning-fragment/src/components/Container.jsx
+++ 4 Fragments/learning-fragment/src/components/Container.jsx
@@ -1,5 +1,5 @@
 import styles from "./Container.module.css"
-
+// Passing children to a component by using props.children:
 const Container = (props) =>{
   return <div className={styles.container}>{props.children}</div>
 }
