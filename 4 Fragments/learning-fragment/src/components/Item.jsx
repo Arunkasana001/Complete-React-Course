a20fb3fb5a0bab03a0456b771d7297e94b6945a8
Arunkasana001 <arunkasana975@gmail.com>
2026-09-04 12:14:24 +0530

    refactor: improve formatting and structure of Item component

--- 4 Fragments/learning-fragment/src/components/Item.jsx
+++ 4 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,13 +1,21 @@
-import styles  from "./Item.module.css";
+import styles from "./Item.module.css";
 
-const Item = ({foodItems}) =>{
-return (
-  <>
-  <li  className={ `${styles['ak-item']} list-group-item`} ><span className={styles['ak-span']} >{foodItems}</span>
-  <button className={`${styles.button} btn btn-info`}>Buy</button>
-  </li>
-  </>  
-)
-
-}
-export default Item; 
+const Item = ({ foodItems }) => {
+  const handleBuyButton = (foodItems) =>{
+console.log(`${foodItems} being bought.`)
+  }
+  return (
+    <>
+      <li className={`${styles["ak-item"]} list-group-item`}>
+        <span className={styles["ak-span"]}>{foodItems}</span>
+        <button
+          className={`${styles.button} btn btn-info`}
+          onClick={() => handleBuyButton(foodItems)}
+        >
+          Buy
+        </button>
+      </li>
+    </>
+  );
+};
+export default Item;
