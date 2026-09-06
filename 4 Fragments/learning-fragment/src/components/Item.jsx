b945cc3f309feeb98745508b0350a7964ecd59b8
Arunkasana001 <arunkasana975@gmail.com>
2026-09-06 08:44:26 +0530

    feat: enhance FoodItems and Item components to manage active state and improve button functionality

--- 4 Fragments/learning-fragment/src/components/Item.jsx
+++ 4 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,6 +1,6 @@
 import styles from "./Item.module.css";
 
-const Item = ({ foodItems }) => {
+const Item = ({ foodItems, bought}) => {
   const handleBuyButton = (event) => {
     console.log(event);
 
@@ -8,7 +8,8 @@ const Item = ({ foodItems }) => {
   };
   return (
     <>
-      <li className={`${styles["ak-item"]} list-group-item`}>
+      <li className={`${styles["ak-item"]} list-group-item ${bought && 'active'} `}
+      >
         <span className={styles["ak-span"]}>{foodItems}</span>
         <button
           className={`${styles.button} btn btn-info`}
