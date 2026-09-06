47d0826ae74dbf5bdc847a5cf0be02d6d2e377e4
Arunkasana001 <arunkasana975@gmail.com>
2026-09-06 08:23:01 +0530

    feat: implement onKeyDown handler to add new food items on Enter key press and clear input field

--- 4 Fragments/learning-fragment/src/App.jsx
+++ 4 Fragments/learning-fragment/src/App.jsx
@@ -18,16 +18,21 @@ function App() {
  
   // let [textToShow, setTextState] = useState(); 
 
- let [foodItems, setFoodItems] =  useState(["Dal",
-    "Green Vegitables",
-    "Roti"]);
+//  let [foodItems, setFoodItems] =  useState(["Dal",
+//     "Green Vegitables",
+//     "Roti"]);
+ let [foodItems, setFoodItems] =  useState([]);
+
   //  { console.log(`Current value of textState: ${textToShow}`);}
 
   const onKeyDown = (event) => {
     if(event.key == 'Enter'){
       let newFoodItem =event.target.value;
+      event.target.value = '';
+
       let newItems = [...foodItems, newFoodItem]
       console.log('Food value entered is ' + newFoodItem);
+      setFoodItems(newItems);
       
     }
     // console.log(event.target.value);
@@ -39,8 +44,8 @@ function App() {
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
 
-        <ErrorMessage items={foodItems}></ErrorMessage>
         <FoodInput handleKeyDown = {onKeyDown} />
+        <ErrorMessage items={foodItems}></ErrorMessage>
         {/* <p>{textToShow}</p> */}
         <FoodItems items={foodItems}></FoodItems>
       </Container>
