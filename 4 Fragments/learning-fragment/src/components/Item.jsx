41956d1b05bf1386c115b9835eae1fce6f0136b2
Arunkasana001 <arunkasana975@gmail.com>
2026-09-06 12:45:19 +0530

    style: format Item component for improved readability

--- 4 Fragments/learning-fragment/src/components/Item.jsx
+++ 4 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,6 +1,6 @@
 import styles from "./Item.module.css";
 
-const Item = ({ foodItems, bought, handleBuyButton}) => {
+const Item = ({ foodItems, bought, handleBuyButton }) => {
   // const handleBuyButton = (event) => {
   //   console.log(event);
 
@@ -8,7 +8,8 @@ const Item = ({ foodItems, bought, handleBuyButton}) => {
   // };
   return (
     <>
-      <li className={`${styles["ak-item"]} list-group-item ${bought && 'active'} `}
+      <li
+        className={`${styles["ak-item"]} list-group-item ${bought && "active"} `}
       >
         <span className={styles["ak-span"]}>{foodItems}</span>
         <button
