0afb14e59fc2521bfa71b5a7f14ad17115a1c587
Arunkasana001 <arunkasana975@gmail.com>
2026-09-07 14:57:03 +0530

    feat: add state management and event handlers for todo name and due date in AddTodo component

--- Projects/6-todo-app-version-3/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-3/src/components/AddTodo.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 function AddTodo({onNewItem}){
+  const [todoName, setTodoName] = useState();
+  const [duedate, setDueDate] = useState();
+
+  const handleNameChange = (event) =>{
+    console.log(event);
+    
+  }
+  const handleDateChange = (event) =>{
+    console.log(event);
+    
+  }
   return (
   <div className="row Ak-row">
-          <div className="col-6"><input type="text" placeholder="Enter Todo here.." /></div>
-          <div className="col-4"><input type="date" /></div>
-          <div className="col-2"><button type="button" class="btn btn-success Ak-button add-btn" onClick={() => onNewItem('a', 'b')}>Add</button></div>
+          <div className="col-6"><input type="text" placeholder="Enter Todo here.." onChange={handleNameChange} /></div>
+          <div className="col-4"><input type="date" onChange={handleDateChange} /></div>
+          <div className="col-2"><button type="button" className="btn btn-success Ak-button add-btn" onClick={() => onNewItem('a', 'b')}>Add</button></div>
         </div>
   )
 }
