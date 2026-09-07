91c5c4cd60b32f52ce7da60a0cfc84699ef9327c
Arunkasana001 <arunkasana975@gmail.com>
2026-09-07 14:59:32 +0530

    refactor: clean up AddTodo component by removing console logs and improving formatting

--- Projects/6-todo-app-version-3/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-3/src/components/AddTodo.jsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
-function AddTodo({onNewItem}){
+function AddTodo({ onNewItem }) {
   const [todoName, setTodoName] = useState();
   const [duedate, setDueDate] = useState();
 
-  const handleNameChange = (event) =>{
-    console.log(event);
-    
-  }
-  const handleDateChange = (event) =>{
-    console.log(event);
-    
-  }
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
   return (
-  <div className="row Ak-row">
-          <div className="col-6"><input type="text" placeholder="Enter Todo here.." onChange={handleNameChange} /></div>
-          <div className="col-4"><input type="date" onChange={handleDateChange} /></div>
-          <div className="col-2"><button type="button" className="btn btn-success Ak-button add-btn" onClick={() => onNewItem('a', 'b')}>Add</button></div>
-        </div>
-  )
+    <div className="row Ak-row">
+      <div className="col-6">
+        <input
+          type="text"
+          placeholder="Enter Todo here.."
+          onChange={handleNameChange}
+        />
+      </div>
+      <div className="col-4">
+        <input type="date" onChange={handleDateChange} />
+      </div>
+      <div className="col-2">
+        <button
+          type="button"
+          className="btn btn-success Ak-button add-btn"
+          onClick={() => onNewItem("a", "b")}
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default AddTodo;
