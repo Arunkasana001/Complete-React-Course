fa08c1a10d1c4c22fb24587a7552f1423f3488be
Arunkasana001 <arunkasana975@gmail.com>
2026-09-07 15:17:35 +0530

    feat: add WelcomeMessage component and display it when no todos are present

--- Projects/6-todo-app-version-3/src/App.jsx
+++ Projects/6-todo-app-version-3/src/App.jsx
@@ -1,28 +1,28 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-
+import WelcomeMessage from "./components/WelcomeMessage";
 import "./App.css";
 
 import TodoItems from "./components/TodoItems";
 import { useState } from "react";
 
 function App() {
-  const initialTodoItems = [
-    {
-      name: "Buy Milk",
-      dueDate: "4/10/2023",
-    },
-    {
-      name: "Go to College",
-      dueDate: "4/10/2023",
-    },
-    {
-      name: "Like this Course",
-      dueDate: "4/10/2025",
-    },
-  ];
+  // const initialTodoItems = [
+  //   {
+  //     name: "Buy Milk",
+  //     dueDate: "4/10/2023",
+  //   },
+  //   {
+  //     name: "Go to College",
+  //     dueDate: "4/10/2023",
+  //   },
+  //   {
+  //     name: "Like this Course",
+  //     dueDate: "4/10/2025",
+  //   },
+  // ];
 
-  const [todoItems, setTodoItems] = useState(initialTodoItems);
+  const [todoItems, setTodoItems] = useState([]);
 
   const onNewItem = (itemName, itemDueDate) => {
     console.log(`New Item Added:${itemName}  Date:${itemDueDate}`);
@@ -37,6 +37,8 @@ function App() {
     <center className="todo-container">
       <AppName />
       <AddTodo onNewItem={onNewItem} />
+      {todoItems.length === 0 &&   <WelcomeMessage></WelcomeMessage>}
+    
       <TodoItems todoItems={todoItems}></TodoItems>
     </center>
   );
