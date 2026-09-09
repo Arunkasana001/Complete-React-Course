45f02d309129c566678d52237d39bee337181aee
Arunkasana001 <arunkasana975@gmail.com>
2026-09-09 08:19:29 +0530

    fix: format code in TodoItems component for better readability

--- Projects/6-todo-app-version-3/src/components/TodoItems.jsx
+++ Projects/6-todo-app-version-3/src/components/TodoItems.jsx
@@ -1,8 +1,8 @@
 import TodoItem from "./TodoItem";
 import styles from "./TodoItems.module.css";
 const TodoItems = ({ todoItems, onDeleteClick }) => {
-  return (
-    <>
+  return ( 
+    <> 
       <div className={styles.itemsContainer}>
         {todoItems.map((item) => (
           <TodoItem key={item.name} todoName={item.name} todoDate={item.dueDate}onDeleteClick = {onDeleteClick}></TodoItem>
