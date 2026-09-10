620cd8213df502044017e053482613eb8926cd40
Arunkasana001 <arunkasana975@gmail.com>
2026-09-10 12:23:19 +0530

    refactor: update App component structure and improve Post component className usage

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
-import CreatePost from './components/CreatePost'
-import Post from './components/Post'
-import Footer from './components/Footer'
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import Post from "./components/Post";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [salectedTab, setSelectedTab] = useState("Home");
 
   return (
-    <div className='app-container'>
-   
-    <Sidebar></Sidebar>
-       <div className='content'>
-    <Header></Header>
-    <CreatePost></CreatePost>
-    <Post></Post>
-    <Footer></Footer>
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="content">
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+        <Footer></Footer>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default App;
