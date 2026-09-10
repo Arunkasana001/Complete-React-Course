15e55c63b619629744ac3f1debc3f50c4064bf6a
Arunkasana001 <arunkasana975@gmail.com>
2026-09-10 09:45:54 +0530

    refactor: implement TodoItemsContextProvider for centralized state management and update App and AddTodo components

--- Projects/6-todo-app-version-3/src/App.jsx
+++ Projects/6-todo-app-version-3/src/App.jsx
@@ -5,24 +5,9 @@ import "./App.css";
 
 import TodoItems from "./components/TodoItems";
 import { useReducer } from "react";
+import TodoItemsContextProvider from "./store/todo-items-store";
 import { TodoItemsContext } from "./store/todo-items-store";
 
-const todoItemsReducer = (currTodoItems, action) => {
-  let newTodoItems = currTodoItems;
-  if (action.type === "NEW_ITEM") {
-    newTodoItems = [
-      ...currTodoItems,
-      { name: action.payload.itemName, dueDate: action.payload.itemDueDate },
-    ];
-  
-  } else if (action.type === "DELETE_ITEM") {
-    newTodoItems = currTodoItems.filter(
-      (item) => item.name !== action.payload.itemName,
-    );
-  }
-
-  return newTodoItems;
-};
 
 function App() {
   // const initialTodoItems = [
@@ -41,17 +26,8 @@ function App() {
   // ];
 
   // const [todoItems, setTodoItems] = useState([]);
-  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
-
-  const addNewItem = (itemName, itemDueDate) => {
-    const newItemAction = {
-      type: "NEW_ITEM",
-      payload: {
-        itemName,
-        itemDueDate,
-      },
-    };
-    dispatchTodoItems(newItemAction);
+  
+ 
     // console.log(`New Item Added:${itemName}  Date:${itemDueDate}`);
     // const newTodoItems = [
     //   ...todoItems,
@@ -62,26 +38,11 @@ function App() {
     //   { name: itemName, dueDate: itemDueDate },
     // ]);
     // setTodoItems(newTodoItems);
-  };
-  const deleteItem = (todoItemName) => {
-    const deleteItemAction = {
-      type: "DELETE_ITEM",
-      payload: {
-        itemName:todoItemName,
-      },
-    };
-      dispatchTodoItems(deleteItemAction);
-  };
 
+ 
 
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems: todoItems,
-        addNewItem: addNewItem,
-        deleteItem: deleteItem,
-      }}
-    >
+  <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo
@@ -96,8 +57,8 @@ function App() {
         // onDeleteClick={handleDeleteItem}
         ></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+</TodoItemsContextProvider>
   );
-}
+};
 
 export default App;
