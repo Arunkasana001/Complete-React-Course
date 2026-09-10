27bebf897f5e74b7c97f6a82b8a5f6f5d7c92a35
Arunkasana001 <arunkasana975@gmail.com>
2026-09-10 06:56:00 +0530

    feat: implement TodoItemsContext for state management and refactor components

--- Projects/6-todo-app-version-3/src/App.jsx
+++ Projects/6-todo-app-version-3/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 
 import TodoItems from "./components/TodoItems";
 import { useState } from "react";
+import { TodoItemsContext } from "./store/todo-items-store";
 
 function App() {
   // const initialTodoItems = [
@@ -24,32 +25,46 @@ function App() {
 
   const [todoItems, setTodoItems] = useState([]);
 
-  const onNewItem = (itemName, itemDueDate) => {
+  const addNewItem = (itemName, itemDueDate) => {
     // console.log(`New Item Added:${itemName}  Date:${itemDueDate}`);
     // const newTodoItems = [
     //   ...todoItems,
     //   { name: itemName, dueDate: itemDueDate },
     // ];
-    setTodoItems((currValue) =>[
-      ...currValue, {name:itemName, dueDate: itemDueDate},
+    setTodoItems((currValue) => [
+      ...currValue,
+      { name: itemName, dueDate: itemDueDate },
     ]);
     // setTodoItems(newTodoItems);
   };
- const handleDeleteItem = (todoItemName) =>{
-  const newTodoItems = todoItems.filter(item => item.name !== todoItemName);
-  setTodoItems(newTodoItems);
-  console.log(`Item Deleted:${todoItemName}`);
-  
-
- }
+  const deleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+    console.log(`Item Deleted:${todoItemName}`);
+  };
   return (
-    <center className="todo-container">
-      <AppName />
-      <AddTodo onNewItem={onNewItem} />
-      {todoItems.length === 0 &&   <WelcomeMessage></WelcomeMessage>}
-    
-      <TodoItems todoItems={todoItems}onDeleteClick ={handleDeleteItem}></TodoItems>
-    </center>
+    <TodoItemsContext.Provider
+      value={{
+        todoItems: todoItems,
+        addNewItem: addNewItem,
+        deleteItem: deleteItem,
+      }}
+    >
+      <center className="todo-container">
+        <AppName />
+        <AddTodo
+        //  onNewItem={onNewItem}
+        />
+        <WelcomeMessage
+        //  todoItems={todoItems}
+        ></WelcomeMessage>
+
+        <TodoItems
+        // todoItems={todoItems}
+        // onDeleteClick={handleDeleteItem}
+        ></TodoItems>
+      </center>
+    </TodoItemsContext.Provider>
   );
 }
 
--- Projects/6-todo-app-version-3/src/components/TodoItems.jsx
+++ Projects/6-todo-app-version-3/src/components/TodoItems.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
+import { TodoItemsContext } from "../store/todo-items-store";
 import TodoItem from "./TodoItem";
 import styles from "./TodoItems.module.css";
-const TodoItems = ({ todoItems, onDeleteClick }) => {
-  return ( 
-    <> 
+const TodoItems = ({  onDeleteClick }) => {
+  const contextObj = useContext(TodoItemsContext);
+  const todoItems = contextObj.todoItems;
+
+  return (
+    <>
       <div className={styles.itemsContainer}>
         {todoItems.map((item) => (
-          <TodoItem key={item.name} todoName={item.name} todoDate={item.dueDate}onDeleteClick = {onDeleteClick}></TodoItem>
+          <TodoItem
+            key={item.name}
+            todoName={item.name}
+            todoDate={item.dueDate}
+            onDeleteClick={onDeleteClick}
+          ></TodoItem>
         ))}
       </div>
     </>
