83564289b7982c64f70d022a24416ef676df6eb1
Arunkasana001 <arunkasana975@gmail.com>
2026-09-12 21:16:07 +0530

    refactor: update Post component to display delete icon in title

--- Projects/7-social-media/src/components/Post.jsx
+++ Projects/7-social-media/src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { MdDeleteOutline } from "react-icons/md";
+
 const Post = ({ post }) => {
   return (
     <div className="card" style={{ width: "18rem" }}>
@@ -6,13 +8,14 @@ const Post = ({ post }) => {
         <h5 className="card-title">
           {post.title}{" "}
           <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {post.reactions}
+            <MdDeleteOutline />
           </span>
         </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
           <span class="badge text-bg-primary hashtag">{tag}</span>
         ))}
+        <div className=""></div>
 
         {/* <a href="#" className="btn btn-primary">
           Go somewhere
