15c4e9b272897ccb1a30a068a25c8c28284a7c26
Arunkasana001 <arunkasana975@gmail.com>
2026-09-12 21:05:51 +0530

    refactor: update Post component to correctly render tags as badges

--- Projects/7-social-media/src/components/Post.jsx
+++ Projects/7-social-media/src/components/Post.jsx
@@ -4,10 +4,11 @@ const Post = ({ post }) => {
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{post.title}</h5>
-        <p className="card-text">
-          {post.body}
-        </p>
-        <span class="badge text-bg-primary">Primary</span>
+        <p className="card-text">{post.body}</p>
+        {post.tags.map((tag) => (
+          <span class="badge text-bg-primary">Primary</span>
+        ))}
+
         {/* <a href="#" className="btn btn-primary">
           Go somewhere
         </a> */}
