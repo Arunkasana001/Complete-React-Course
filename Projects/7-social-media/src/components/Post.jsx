d4fbb0a967a8e81a3d92af5adfe313b4497aa75e
Arunkasana001 <arunkasana975@gmail.com>
2026-09-12 21:10:54 +0530

    refactor: enhance Post component to display reactions prominently in the title

--- Projects/7-social-media/src/components/Post.jsx
+++ Projects/7-social-media/src/components/Post.jsx
@@ -3,7 +3,12 @@ const Post = ({ post }) => {
     <div className="card" style={{ width: "18rem" }}>
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">{post.title}</h5>
+        <h5 className="card-title">
+          {post.title}{" "}
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {post.reactions}
+          </span>
+        </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
           <span class="badge text-bg-primary hashtag">{tag}</span>
