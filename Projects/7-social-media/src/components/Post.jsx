226d8e8ea3602e3d0798c031f0022f3b4c3f8f56
Arunkasana001 <arunkasana975@gmail.com>
2026-09-13 15:13:25 +0530

    refactor: update Post component to display reactions count in alert

--- Projects/7-social-media/src/components/Post.jsx
+++ Projects/7-social-media/src/components/Post.jsx
@@ -15,7 +15,10 @@ const Post = ({ post }) => {
         {post.tags.map((tag) => (
           <span class="badge text-bg-primary hashtag">{tag}</span>
         ))}
-        <div className="alert alert-success reactions" role="alert"> A simple sucess alert-check it out !</div>
+        <div className="alert alert-success reactions" role="alert">
+          {" "}
+          This post has been reacted by {post.reactions} people.
+        </div>
 
         {/* <a href="#" className="btn btn-primary">
           Go somewhere
