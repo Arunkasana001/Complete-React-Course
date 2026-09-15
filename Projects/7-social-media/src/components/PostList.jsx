e6545c57426348daf71fdb394dca7e0938c44af3
Arunkasana001 <arunkasana975@gmail.com>
2026-09-15 17:53:26 +0530

    refactor: clean up PostList component formatting and improve handleGetPostClick function

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -4,19 +4,20 @@ import { PostList as PostListData } from "../store/post-list-store";
 import WelocomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-
   const { postList } = useContext(PostListData);
 
-  const handleGetPostClick = () =>{
-console.log('get post clicked.');
-
-  }
-
+  const handleGetPostClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then(console.log);
+  };
   return (
     <>
-    {postList.length === 0 && <WelocomeMessage onGetPostClick={handleGetPostClick} /> }
+      {postList.length === 0 && (
+        <WelocomeMessage onGetPostClick={handleGetPostClick} />
+      )}
       {postList.map((post) => (
-        <Post key={post.id} post={post}/>
+        <Post key={post.id} post={post} />
       ))}
     </>
   );
