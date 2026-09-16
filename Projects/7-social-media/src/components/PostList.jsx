0308873053641cc56e3d662d179d476ecdae98a7
Arunkasana001 <arunkasana975@gmail.com>
2026-09-16 20:19:18 +0530

    feat: add state management for data fetching in PostList component

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelocomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [dataFetched, setDataFetched] = useState(false);
 
    fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
