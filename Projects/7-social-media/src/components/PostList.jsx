250a0127f827b062cc0e7e620062c9d8ae8181d4
Arunkasana001 <arunkasana975@gmail.com>
2026-09-17 09:03:54 +0530

    refactor: add missing useEffect import and clean up PostList component

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -1,21 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelocomeMessage from "./WelcomeMessage";
- 
+
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-    setDataFetched(true);
-
+  }, []);
 
   // const handleGetPostClick = () => {};
-  return ( 
+  return (
     <>
       {postList.length === 0 && (
         <WelocomeMessage
