8902ab0fe445e0d0d33d49b151fcb8a7ec0ce34c
Arunkasana001 <arunkasana975@gmail.com>
2026-09-19 16:46:05 +0530

    refactor: restructure App component and styles; move App.jsx and App.css to routes directory

--- Projects/7-social-media/src/routes/App.jsx
+++ Projects/7-social-media/src/routes/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 import "./App.css";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import CreatePost from "./components/CreatePost";
-import PostList from "./components/PostList";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import CreatePost from "../components/CreatePost";
+import PostList from "../components/PostList";
+import Footer from "../components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PostListProvider from "./store/post-list-store";
+import PostListProvider from "../store/post-list-store";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Create Post");
