3e55f0cd6edaa78a560a4ea68355e440bb82e207
Arunkasana001 <arunkasana975@gmail.com>
2026-09-21 13:30:36 +0530

    feat: implement form structure in CreatePost component

--- Projects/7-social-media/src/components/CreatePost.jsx
+++ Projects/7-social-media/src/components/CreatePost.jsx
@@ -25,6 +25,77 @@ const CreatePost = () => {
     reactionsElement.current.value = "";
     tagsElement.current.value = "";
 
+    return (
+      <form className="create-post" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="userId" className="form-label">
+            Enter your User Id here
+          </label>
+          <input
+            type="text"
+            ref={userIdElement}
+            className="form-control"
+            id="userId"
+            placeholder="Your user ID"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Post Title
+          </label>
+          <input
+            type="text"
+            ref={postTitleElement}
+            className="form-control"
+            id="title"
+            placeholder="How are you feeling today..."
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="body" className="form-label">
+            Post Content
+          </label>
+          <textarea
+            type="text"
+            ref={postBodyElement}
+            rows={4}
+            className="form-control"
+            id="title"
+            placeholder="Tell us more about it"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Number of reactions" className="form-label">
+            Number of Reactions
+          </label>
+          <input
+            type="text"
+            ref={reactionsElement}
+            className="form-control"
+            id="reactions"
+            placeholder="How many people reacted to this post"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="tags" className="form-label">
+            Enter your hashtags here
+          </label>
+          <input
+            type="text"
+            ref={tagsElement}
+            className="form-control"
+            id="tags"
+            placeholder="Please enter  tags using space"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Post
+        </button>
+      </form>
+    );
+  };
+  export const createPostAction = () => {
     fetch("https://dummyjson.com/posts/add", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -45,77 +116,5 @@ const CreatePost = () => {
       });
     navigate("/");
   };
-  return (
-    <form className="create-post" onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="userId" className="form-label">
-          Enter your User Id here
-        </label>
-        <input
-          type="text"
-          ref={userIdElement}
-          className="form-control"
-          id="userId"
-          placeholder="Your user ID"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Post Title
-        </label>
-        <input
-          type="text"
-          ref={postTitleElement}
-          className="form-control"
-          id="title"
-          placeholder="How are you feeling today..."
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="body" className="form-label">
-          Post Content
-        </label>
-        <textarea
-          type="text"
-          ref={postBodyElement}
-          rows={4}
-          className="form-control"
-          id="title"
-          placeholder="Tell us more about it"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="Number of reactions" className="form-label">
-          Number of Reactions
-        </label>
-        <input
-          type="text"
-          ref={reactionsElement}
-          className="form-control"
-          id="reactions"
-          placeholder="How many people reacted to this post"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="tags" className="form-label">
-          Enter your hashtags here
-        </label>
-        <input
-          type="text"
-          ref={tagsElement}
-          className="form-control"
-          id="tags"
-          placeholder="Please enter  tags using space"
-        />
-      </div>
-
-      <button type="submit" className="btn btn-primary">
-        Post
-      </button>
-    </form>
-  );
 };
-export const  createPostAction = () =>{
-  
-}
 export default CreatePost;
