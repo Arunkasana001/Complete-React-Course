c1910e8cd936e0e302678d12dd4d1a1b64cd2205
Arunkasana001 <arunkasana975@gmail.com>
2026-09-21 14:04:43 +0530

    feat: refactor CreatePost component to use Form and handle form submission

--- Projects/7-social-media/src/components/CreatePost.jsx
+++ Projects/7-social-media/src/components/CreatePost.jsx
@@ -1,6 +1,6 @@
 import { useContext, useRef } from "react";
 import { PostList } from "../store/post-list-store";
-import { useNavigate } from "react-router-dom";
+import { Form, redirect, useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
   // const navigate = useNavigate();
@@ -12,114 +12,108 @@ const CreatePost = () => {
   // const tagsElement = useRef();
 
   // const handleSubmit = (event) => {
-    // event.preventDefault();
-    // const userId = userIdElement.current.value;
-    // const postTitle = postTitleElement.current.value;
-    // const postBody = postBodyElement.current.value;
-    // const reactions = Number(reactionsElement.current.value) || 0;
-    // const tags = tagsElement.current.value.split(" ");
+  // event.preventDefault();
+  // const userId = userIdElement.current.value;
+  // const postTitle = postTitleElement.current.value;
+  // const postBody = postBodyElement.current.value;
+  // const reactions = Number(reactionsElement.current.value) || 0;
+  // const tags = tagsElement.current.value.split(" ");
 
-    // userIdElement.current.value = "";
-    // postTitleElement.current.value = "";
-    // postBodyElement.current.value = "";
-    // reactionsElement.current.value = "";
-    // tagsElement.current.value = "";
+  // userIdElement.current.value = "";
+  // postTitleElement.current.value = "";
+  // postBodyElement.current.value = "";
+  // reactionsElement.current.value = "";
+  // tagsElement.current.value = "";
 
-    return (
-      <Form
-        method="POST"
-        className="create-post"
-        //  onSubmit={handleSubmit}
-      >
-        <div className="mb-3">
-          <label htmlFor="userId" className="form-label">
-            Enter your User Id here
-          </label>
-          <input
-            type="text"
-            name="userId"
-            className="form-control"
-            id="userId"
-            placeholder="Your user ID"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="title" className="form-label">
-            Post Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            className="form-control"
-            id="title"
-            placeholder="How are you feeling today..."
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="body" className="form-label">
-            Post Content
-          </label>
-          <textarea
-            type="text"
-            name="body"
-            rows={4}
-            className="form-control"
-            id="title"
-            placeholder="Tell us more about it"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="Number of reactions" className="form-label">
-            Number of Reactions
-          </label>
-          <input
-            type="text"
-            name="Number of reactions"
-            className="form-control"
-            id="reactions"
-            placeholder="How many people reacted to this post"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="tags" className="form-label">
-            Enter your hashtags here
-          </label>
-          <input
-            type="text"
-            name="tags"
-            className="form-control"
-            id="tags"
-            placeholder="Please enter  tags using space"
-          />
-        </div>
+  return (
+    <Form
+      method="POST"
+      className="create-post"
+      //  onSubmit={handleSubmit}
+    >
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your User Id here
+        </label>
+        <input
+          type="text"
+          name="userId"
+          className="form-control"
+          id="userId"
+          placeholder="Your user ID"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="form-control"
+          id="title"
+          placeholder="How are you feeling today..."
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          name="body"
+          rows={4}
+          className="form-control"
+          id="title"
+          placeholder="Tell us more about it"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="Number of reactions" className="form-label">
+          Number of Reactions
+        </label>
+        <input
+          type="text"
+          name="Number of reactions"
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to this post"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags here
+        </label>
+        <input
+          type="text"
+          name="tags"
+          className="form-control"
+          id="tags"
+          placeholder="Please enter  tags using space"
+        />
+      </div>
 
-        <button type="submit" className="btn btn-primary">
-          Post
-        </button>
-      </Form>
-    );
-  };
-  export const createPostAction = (data) => {
-   const formData = await data.request.formData()
-    fetch("https://dummyjson.com/posts/add", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: postTitle,
-        body: postBody,
-        reactions: {
-          likes: reactions,
-          dislikes: 0,
-        },
-        userId: userId,
-        tags: tags,
-      }),
-    })
-      .then((res) => res.json())
-      .then((post) => {
-        addPost(post);
-      });
-    navigate("/");
-  };
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </Form>
+  );
+};
+export async function createPostAction(data) {
+  const formData = await data.request.formData();
+  const postData = Object.fromEntries(formData);
+  postData.tags = postData.tags.split("");
+  fetch("https://dummyjson.com/posts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: postData,
+  })
+    .then((res) => res.json())
+    .then((post) => {
+      addPost(post);
+    });
+  // navigate("/");
+  return redirect("/");
+}
 // };
 export default CreatePost;
