ae011c496bc561fd6b2f6ab7c142b53f7dbffd90
Arunkasana001 <arunkasana975@gmail.com>
2026-09-23 13:45:03 +0530

    feat: implement increment and decrement functionality in Redux store and update UI components

--- 52 Redux/Counter-React-Redux/src/components/DisplayCounter.jsx
+++ 52 Redux/Counter-React-Redux/src/components/DisplayCounter.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+
 const DisplayCounter = () => {
-  return <p className="lead mb-4">Counter current Value</p>;
+  const counter = useSelector((store) => store.counter);
+  return <p className="lead mb-4">Counter current value: {counter}</p>;
 };
 
 export default DisplayCounter;
