0bb2dbb84f90d03977ae1be9772d033d6221a13f
Arunkasana001 <arunkasana975@gmail.com>
2026-09-25 10:26:28 +0530

    Add initial HTML structure and JavaScript functionality for Myntra clone
    - Created index.html with header, footer, and main sections.
    - Added bag.html for the shopping bag page with a summary section.
    - Implemented bag.js to manage bag items, calculate totals, and display item details.
    - Developed index.js to handle item display on the homepage and manage bag item count.
    - Included CSS links for styling and Google Fonts for icons.

--- 4 Fragments/learning-fragment/src/components/Container.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Container.module.css";
-// Passing children to a component by using props.children:
-const Container = (props) => {
-  return <div className={styles.container}>{props.children}</div>;
-};
-
-export default Container;
